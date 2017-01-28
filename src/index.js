48aceeedbaf97bd034386cf37e16f589758fcd1a
Thomas Van Dort <vanbarrington@gmail.com>
2017-01-28 14:34:58 -0500

    messing with what bot can hear

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import './exit';
 import config from './config';
+import Botkit from 'botkit';
+//console.log(process.env.test);
 
-console.log(process.env.test);
+let token = config.slackToken || process.env.slack_token;
+
+let controller = Botkit.slackbot({
+    debug: false
+});
+
+controller.spawn({
+    token: token
+}).startRTM();
+
+controller.hears(['test', 'hey memebot'], ['ambient', 'message_received', 'direct_mention', 'direct_message'], function(bot, message) {    
+    console.log(message);
+    bot.reply(message, 'what the fuck u want');
+});
 
 process.on("SIGINT", function () {
   //graceful shutdown
